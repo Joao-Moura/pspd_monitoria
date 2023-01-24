64e9a9e220310cd1c5c9d9b322eeb7ba55b18461
Ubuntu <thiagohdaqw@gmail.com>
2023-01-24 00:02:28 +0000

    Adicionando script de criacao de usuario do hadoop
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

---
+++ cluster/create_hadoop_user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERNAME=$1
+QUOTA_SIZE=${2:-1g}
+
+user_path=/user/$USERNAME
+
+hdfs dfs -mkdir -p $user_path
+hdfs dfs -chown $USERNAME:$USERNAME $user_path
+
+if [[ $QUOTA_SIZE -gt 0 ]]; then
+	hdfs dfsadmin -setSpaceQuota $QUOTA_SIZE $user_path
+fi
