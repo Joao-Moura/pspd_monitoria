df4294eeb8bd3e2165113244f4df5f84d6788544
Ubuntu <thiagohdaqw@gmail.com>
2023-01-24 00:40:23 +0000

    Permissionando pasta do usuario hadoop
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>

--- cluster/create_hadoop_user.sh
+++ cluster/create_hadoop_user.sh
@@ -7,6 +7,7 @@ user_path=/user/$USERNAME
 
 hdfs dfs -mkdir -p $user_path
 hdfs dfs -chown $USERNAME:$USERNAME $user_path
+hdfs dfs -chmod 700 $user_path
 
 if [[ $QUOTA_SIZE -gt 0 ]]; then
 	hdfs dfsadmin -setSpaceQuota $QUOTA_SIZE $user_path
