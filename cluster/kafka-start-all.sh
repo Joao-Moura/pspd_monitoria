05b99f2fe786ea037db8967db24a2c8b7fa9fda5
thiagohdaqw <thiagohdaqw@gmail.com>
2023-05-09 19:35:11 -0300

    atualizando scripts do cluster

--- cluster/kafka-start-all.sh
+++ cluster/kafka-start-all.sh
@@ -4,6 +4,8 @@ SOURCE_ENV="source bin/chococino_env;"
 
 ssh cm2 "$SOURCE_ENV zookeeper-server-start.sh -daemon kafka/kafka-cm2/config/zookeeper.properties"
 
+sleep 5
+
 for hostname in cm{1,2,3,4} gpu{1,2,3}; do
 	ssh $hostname "$SOURCE_ENV kafka-server-start.sh -daemon $HADOOP_PROF_HOME/kafka/kafka-$hostname/config/server.properties"
 done
